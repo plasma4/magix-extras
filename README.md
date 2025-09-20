7f68cfd911e1e1993e56fae68801463ebcf085f2
plasma4 <72562754+plasma4@users.noreply.github.com>
2025-09-19 22:48:08 -0500

    Added hotkeys script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Magix Extras
 ### Some mods may require or work better with Magix.
-They have been made to work along with the fixed version of Magix, which you can figure out how to install over [here](https://github.com/plasma4/magix-fix/blob/main/README.md). The original creator, pelletsstarPL, has abandoned Magix and these Magix mods for the time being.
+These modes have been made to work along with the fixed version of Magix, which you can figure out how to install over [here](https://github.com/plasma4/magix-fix/blob/main/README.md). The original creator, pelletsstarPL, has abandoned Magix and these Magix mods for the time being.
 
 ## Normal installation
 **In order to install these Magix mods, use the links below:**
@@ -47,3 +47,6 @@ You have two options:
 To get them installed through links, you can do it in a similar fashion. Click on Use Magix, add the link or links at the end, and copy all of the text **without deleting the original 2 Magix files**. You'll need to reload the page in order to actually install them; you can't just simply Load Mods.
 
 However, to locally copy them over, you'll need to manually get the sprite sheet out and download the files from the links. Then, you can import it directly (without the starting part of the url, so, for example, the Thot mod would just become `thot.js` when imported).
+
+## Keyboard shortcuts
+The script for hotkeys (https://plasma4.github.io/magix-extras/thot.js) is already in Magix by default so you don't need to add it, but can be installed with mods other than Magix if you wish as described above.
---
+++ hotkeys.js
@@ -0,0 +1,56 @@
+var _shortcutsLoaded = false
+G.AddData({
+    name: 'Shortcuts',
+    author: '1_e0',
+    desc: 'Adds some simple QOL hotkeys to the game. (0 to reroll research, 1-9 to select research, hold Alt for temporary fast forward, and _ or - and = or + act as the multiply/divide by 10 for unit selection amount.',
+    engineVersion: 1,
+    manifest: 0,
+    func: function () {
+        // Custom implementation of keyboard events
+        if (window.loadedMagix || _shortcutsLoaded) {
+            return
+        }
+        _shortcutsLoaded = true
+        document.addEventListener("keydown", function (e) {
+            var key = e.key
+            if (key >= '0' && key <= '9') {
+                if (G.tab.id == 'tech') {
+                    pressed = parseInt(key)
+                    var chooseBox = G.chooseBox[0]
+                    if (pressed === 0) {
+                        if (G.speed > 0) { G.rerollChooseBox(chooseBox); } else G.cantWhenPaused();
+                    } else if (pressed <= chooseBox.choices.length) {
+                        if (G.speed > 0) { G.purchaseChooseBox(chooseBox, G.chooseBox[0].choices[pressed - 1], l("chooseOption-" + pressed + "-0")); } else G.cantWhenPaused();
+                    }
+                }
+            } else if (key == "_" || key == "-") {
+                if (G.tab.id == 'unit') l('removeBulk').click()
+                if (G.tab.id == 'land' && G.mapZoomT == 2) {
+                    G.mapZoomT = 1;
+                    G.mapOffXT /= 2;
+                    G.mapOffYT /= 2;
+                    G.tooltip.close();
+                }
+            } else if (key == "+" || key == "=") {
+                if (G.tab.id == 'unit') l('addBulk').click()
+                if (G.tab.id == 'land' && G.mapZoomT == 1) {
+                    G.mapZoomT = 2;
+                    G.mapOffXT *= 2;
+                    G.mapOffYT *= 2;
+                    G.tooltip.close();
+                }
+                G.Scroll = 1;
+            } else if (e.altKey) {
+                l("fastButton").click()
+            }
+        })
+        document.addEventListener("keyup", function (e) {
+            if (e.key === "Alt") {
+                l("playButton").click()
+            }
+        })
+        document.addEventListener("blur", function () {
+            l("playButton").click()
+        })
+    }
+})
